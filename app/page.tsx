0a2620dee575a569355ba4bb402f2fd820a50c47
ID-JA <idaissa93@gmail.com>
2023-07-17 21:58:14 +0100

    feat(app): Add shadcn UI and override colors palette

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
-		<main className="flex flex-col items-center justify-between min-h-screen p-24">
+		<main>
+			<Button variant="default">Get Started</Button>
+			<div className="max-w-md px-4 py-3 rounded-md bg-primary-50">
+				<h1 className="font-semibold text-primary-500">Alert</h1>
+				<p className="text-">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate
+					quidem sequi illo. Quisquam, voluptatum. Quisquam, voluptatum.
+				</p>
+			</div>
 		</main>
 	)
 }
